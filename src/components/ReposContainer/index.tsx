2580c032e4d36f0dc0d4a72853573302c1591d13
João Pedro Leopoldino <leopoldino26@gmail.com>
2021-10-13 10:29:38 -0300

    feat: started an repos container component

---
+++ src/components/ReposContainer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { useUser } from '../../hooks/user'
+
+const ReposContainer: React.FC = ({}) => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default ReposContainer;
