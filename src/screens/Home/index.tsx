70b4eb201ab8145ec5449b3766667beb9286a208
João Pedro Leopoldino <leopoldino26@gmail.com>
2021-10-18 23:01:29 -0300

    feat: added the search functionality and changed some styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,7 @@ import animationData from '../../assets/loading.json';
 import * as SC from './styles';
 import { Card, ReposCard } from '../../components/Cards';
 
-import { useUser } from '../../hooks/user';
+import { useUser, ReposData } from '../../hooks/user';
 import { Redirect } from 'react-router-dom';
 
 const Home: React.FC = () => {
@@ -13,7 +13,20 @@ const Home: React.FC = () => {
     const { getAllUserInfo, userData, reposData, userLogon, loading, reposLanguagesData } = useUser();
     const [redirect, setRedirect] = useState(false);
 
-    const RepositoriesData = { reposData, reposLanguagesData }
+    // const [allData,setAllData] = useState([]);
+    const [filteredRepos, setFilteredRepos] = useState<ReposData[]>(reposData);
+
+    const RepositoriesData = { filteredRepos, reposLanguagesData }
+
+    const handleSearch = (event: any) => {
+        let value = event.target.value.toLowerCase();
+        let result = [];
+        console.log(value);
+        result = reposData.filter((item) => {
+            return item.name.search(value) !== -1;
+        });
+        setFilteredRepos(result);
+    }
 
     const defaultOptions = {
         loop: true,
@@ -36,41 +49,206 @@ const Home: React.FC = () => {
         checkHaveUser();
     }, [getAllUserInfo, userLogon])
 
+    useEffect(() => {
+        const checkFilterRepos = () => {
+            if(filteredRepos.length > 0){
+                setFilteredRepos(reposData)
+            } else {
+                return;
+            }
+        }
+        checkFilterRepos();
+        
+    }, [setFilteredRepos, reposData])
+
     return(
         redirect
         ? <Redirect to="/" />
         : <SC.Container>
             { loading
-            ? <Lottie
-                options={defaultOptions}
-                height={200}
-                width={200}
-            />
+            ? <SC.LoadingContainer>
+                <Lottie
+                    options={defaultOptions}
+                    height={200}
+                    width={200}
+                />
+            </SC.LoadingContainer>
             : <>
             {/* <SC.Title>Home</SC.Title> */}
             <Card
-                width={30}
+                width={25}
                 height={50}
             >
                 <SC.ArrowLeft onClick={() => setRedirect(!redirect)} />
-                <a href="https://github.com/settings/profile" target="_blank" rel="noreferrer">
+                <SC.LinkContainer href="https://github.com/settings/profile" target="_blank" rel="noreferrer">
                     <SC.UserAvatar src={userData.avatar_url}/>
-                </a>
-                <SC.UserName>{userData.name}</SC.UserName>
-                <SC.UserName>{userData.login}</SC.UserName>
-                <SC.UserName>{userData.location}</SC.UserName>
-                <SC.UserName>{userData.company}</SC.UserName>
-                <SC.UserName>{userData.email}</SC.UserName>
-                <SC.UserName>{`Member since: ${userData.created_at.substr(0, 4)}`}</SC.UserName>
+                </SC.LinkContainer>
+                <SC.Column>
+                    <SC.UserName>{userData.name}</SC.UserName>
+                    <SC.UserLinkRow
+                        href={userData.html_url}
+                        target="_blank"
+                    >
+                        <SC.GitIcon/>
+                        <SC.Text>{userData.login}</SC.Text>
+                    </SC.UserLinkRow>
+                </SC.Column>
+                <SC.Divider/>
+                <SC.Row>
+                    <SC.UsersIcon/>
+                    <SC.BoldText>{userData.followers}</SC.BoldText>
+                    <SC.Text>followers · </SC.Text>
+                    <SC.BoldText>{userData.following}</SC.BoldText>
+                    <SC.Text>following</SC.Text>
+                </SC.Row>
+                {
+                    userData.bio
+                    ? <SC.Row>
+                        <SC.QuoteIcon/>
+                        <SC.Text>{userData.bio}</SC.Text>
+                    </SC.Row>
+                    : null
+                }
+                {
+                    userData.company
+                    ? <SC.Row>
+                        <SC.CompanyIcon/>
+                        <SC.BoldText>{userData.company}</SC.BoldText>
+                    </SC.Row>
+                    : null
+                }
+                {
+                    userData.location
+                    ? <SC.Row>
+                        <SC.LocationIcon/>
+                        <SC.BoldText>{userData.location}</SC.BoldText>
+                    </SC.Row>
+                    : null
+                }
+                {
+                    userData.email
+                    ? <SC.LinkRow
+                        href={`mailto:${userData.email}`}
+                        target="_blank"
+                    >
+                        <SC.EmailIcon/>
+                        <SC.BoldText>{userData.email}</SC.BoldText>
+                    </SC.LinkRow>
+                    : null
+                }
+                {
+                    userData.blog
+                    ? <SC.LinkRow
+                        href={userData.blog}
+                        target="_blank"
+                    >
+                        <SC.BlogIcon/>
+                        <SC.BoldText>{userData.blog}</SC.BoldText>
+                    </SC.LinkRow>
+                    : null
+                }
+                {
+                    userData.twitter_username
+                    ? <SC.LinkRow
+                        href={`https://twitter.com/${userData.twitter_username}`}
+                        target="_blank"
+                    >
+                        <SC.TwitterIcon/>
+                        <SC.BoldText>{userData.twitter_username}</SC.BoldText>
+                    </SC.LinkRow>
+                    : null
+                }
+                <SC.Text>{`Member since: ${userData.created_at.substr(0, 4)}`}</SC.Text>
             </Card>
             
-            <ReposCard
-                width={80}
-                height={50}
-                data={RepositoriesData}
-            />
+            <SC.Column>
+                <Card
+                    width={60}
+                    height={7.75}
+                >   
+                    <SC.SimpleRow>
+                        <SC.LinkTabRowActive
+                            href="#/home"
+                        >
+                            <SC.OverviewIcon/>
+                            <SC.BoldText>Overview</SC.BoldText>
+                        </SC.LinkTabRowActive>
+                        <SC.LinkTabRow
+                            href={`https://github.com/${userLogon}?tab=repositories`}
+                            target="_blank"
+                        >
+                            <SC.RepositoriesIcon/>
+                            <SC.BoldText>Repositories</SC.BoldText>
+                        </SC.LinkTabRow>
+                        <SC.LinkTabRow
+                            href={`https://github.com/${userLogon}?tab=projects`}
+                            target="_blank"
+                        >
+                            <SC.ProjectsIcon/>
+                            <SC.BoldText>Projects</SC.BoldText>
+                        </SC.LinkTabRow>
+                        <SC.LinkTabRow
+                            href={`https://github.com/${userLogon}?tab=packages`}
+                            target="_blank"
+                        >
+                            <SC.PackagesIcon/>
+                            <SC.BoldText>Packages</SC.BoldText>
+                        </SC.LinkTabRow>
+                    </SC.SimpleRow>
+                    <SC.Input
+                        type="text"
+                        placeholder="Search a repository"
+                        onChange={handleSearch}
+                    />
+                </Card>
+                <ReposCard
+                    width={60}
+                    height={41.75}
+                    data={RepositoriesData}
+                />
+            </SC.Column>
             </>
             }
+            <SC.SvgBG
+                id="visual"
+                viewBox="0 0 960 540"
+                // width="960"
+                // height="540"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+            >
+                <path d="M0 109L192 66L384 125L576 60L768 93L960 77L960 0L768 0L576 0L384 0L192 0L0 0Z" fill="#230aad">
+                </path>
+                <path d="M0 152L192 249L384 201L576 249L768 163L960 271L960 75L768 91L576 58L384 123L192 64L0 107Z" fill="#3115b6">
+                </path>
+                <path d="M0 217L192 320L384 287L576 293L768 249L960 298L960 269L768 161L576 247L384 199L192 247L0 150Z" fill="#3d1ebf">
+                </path>
+                <path d="M0 406L192 379L384 422L576 390L768 455L960 417L960 296L768 247L576 291L384 285L192 318L0 215Z" fill="#4826c7">
+                </path>
+                <path d="M0 428L192 460L384 492L576 444L768 503L960 487L960 415L768 453L576 388L384 420L192 377L0 404Z" fill="#532ed0">
+                </path>
+                <path d="M0 541L192 541L384 541L576 541L768 541L960 541L960 485L768 501L576 442L384 490L192 458L0 426Z" fill="#5d36d9">
+                </path>
+            </SC.SvgBG>
+            {/* <SC.SvgBG
+                id="visual"
+                viewBox="0 0 960 540"
+                width="100vw"
+                height="100vh"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                // style={{
+                //     position: 'absolute',
+
+                // }}
+            >
+                <g fill="rgba(18,11,129,1)">
+                    <circle r="273" cx="439" cy="526"></circle>
+                    <circle r="188" cx="928" cy="95"></circle>
+                    <circle r="246" cx="185" cy="42"></circle>
+                </g>
+            </SC.SvgBG> */}
         </SC.Container>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components';
-import { FiArrowLeft } from "react-icons/fi";
+import {
+    FiArrowLeft,
+    FiUsers,
+    FiStar,
+    FiBriefcase,
+    FiMapPin,
+    FiMail,
+    FiGlobe,
+    FiGithub,
+    FiTwitter,
+    FiSun,
+    FiMoon,
+
+    FiBookOpen,
+    FiBook,
+    FiLayout,
+    FiBox,
+} from "react-icons/fi";
+import { FaQuoteLeft } from "react-icons/fa";
+
+export const LoadingContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0,0,0,.25);
+    backdrop-filter: saturate(180%) blur(10px);
+    z-index: 99;
+`;
 
 export const Container = styled.div`
     width: 100vw;
@@ -8,13 +37,17 @@ export const Container = styled.div`
     position: relative;
     display: flex;
     flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
     /* background-color: #17102D; */
     background: rgb(13,9,24);
     background: linear-gradient(0deg, rgba(13,9,24,1) 0%, rgba(25,17,51,1) 100%);
+    box-shadow: inset 0 0 8em rgba(0, 0, 0, 1);
     z-index: 0;
     /* background-color: #0d0f17; */
 
-    &::after{
+    /* &::after{
         content: '';
         position: absolute;
         top: -50px;
@@ -36,8 +69,8 @@ export const Container = styled.div`
         border-radius: 100%;
         background: rgb(18,11,129);
         background: linear-gradient(0deg, rgba(18,11,129,1) 0%, rgba(33,5,126,1) 49%, rgba(145,23,232,1) 100%);
-    }
-    `;
+    } */
+`;
 
 export const Title = styled.h1`
     color: #FFF;
@@ -103,16 +136,229 @@ export const RepoRow = styled.a`
     }
 `;
 
-export const UserName = styled.p`
-    color: #FFF;
-    font-size: 18px;
+export const LinkContainer = styled.a`
+    width: 85%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const UserAvatar = styled.img`
-    width: 200px;
+    width: 100%;
     height: auto;
     border-radius: 100%;
     border: 2px solid #90a0d1;
+`;
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    gap: 10px;
+    margin: 0;
+`;
+
+export const FollowersRow = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.8rem;
+    border-radius: 8px;
+    background-color: rgba(144, 160, 209, 0.11);
+`;
+
+export const RowClean = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+`;
+
+export const Row = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.5rem 0.9rem;
+    border-radius: 6px;
+    background-color: rgba(144, 160, 209, 0.11);
+`;
+
+export const SimpleRow = styled.div`
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: center;
+    gap: 24px;
+    margin: 0;
+    padding: 0.5rem 0.9rem;
+    border-radius: 6px;
+`;
+
+export const LinkRow = styled.a`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.5rem 0.9rem;
+    border-radius: 6px;
+    background-color: rgba(144, 160, 209, 0.11);
+    cursor: pointer;
+    z-index: 99;
+    text-decoration: none;
+    transition: background-color 0.2s ease;
+
+    &:hover{
+        background-color: rgba(144, 160, 209, 0.2);
+    }
+    &:active{
+        background-color: rgba(144, 160, 209, 0.5);
+    }
+`;
+
+export const LinkTabRow = styled.a`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.5rem;
+    border-radius: 6px;
+    background-color: rgba(144, 160, 209, 0.11);
+    cursor: pointer;
+    z-index: 99;
+    text-decoration: none;
+    transition: background-color 0.2s ease;
+
+    &:hover{
+        background-color: rgba(144, 160, 209, 0.2);
+    }
+    &:active{
+        background-color: rgba(144, 160, 209, 0.5);
+    }
+`;
+
+export const LinkTabRowActive = styled.a`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.5rem;
+    border-radius: 6px;
+    background-color: rgba(144, 160, 209, 0.35);
+    cursor: pointer;
+    z-index: 99;
+    text-decoration: none;
+    transition: background-color 0.2s ease;
+
+    &:active{
+        background-color: rgba(144, 160, 209, 0.5);
+    }
+`;
+
+export const UserLinkRow = styled.a`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    align-self: start;
+    gap: 8px;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0.4rem 0.8rem;
+    border-radius: 6px;
+    background-color: rgba(144, 160, 209, 0.11);
+    cursor: pointer;
+    z-index: 99;
+    text-decoration: none;
+    transition: background-color 0.2s ease;
+
+    &:hover{
+        background-color: rgba(144, 160, 209, 0.2);
+    }
+    &:active{
+        background-color: rgba(144, 160, 209, 0.5);
+    }
+`;
+
+export const Divider = styled.span`
+    width: 100%;
+    height: 1px;
+    border-radius: 100%;
+    background-color: #90a0d1;
+    margin: 0;
+`;
+
+
+export const BoldText = styled.b`
+    color: #FFF;
+`;
+
+export const Text = styled.p`
+    color: #FFF;
+`;
+
+export const LinkText = styled.a`
+    color: #FFF;
+    font-weight: bold;
+    cursor: pointer;
+    z-index: 99;
+    text-decoration: none;
+    transition: color 0.15s ease;
+
+    &:hover{
+        color: #90a0d1;
+    }
+    &:active{
+        color: #FFF;
+    }
+`;
+
+export const UserName = styled.p`
+    color: #FFF;
+    font-size: 28px;
+    font-weight: bold;
+    text-transform: capitalize;
+    margin: 0;
+`;
+
+export const UserLogin = styled.p`
+    color: #FFF;
+    font-size: 18px;
+    margin: 0;
 `;
 
 export const ArrowLeft = styled(FiArrowLeft)`
@@ -122,7 +368,7 @@ export const ArrowLeft = styled(FiArrowLeft)`
     padding: 14px;
     cursor: pointer;
     color: #FFF;
-    font-size: 24px;
+    font-size: 28px;
     border-radius: 18px;
     transition: 0.4s ease;
 
@@ -132,5 +378,101 @@ export const ArrowLeft = styled(FiArrowLeft)`
 
     &:active{
         box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.5);
+    }
+`;
+
+export const QuoteIcon = styled(FaQuoteLeft)`
+    font-size: 13px;
+    color: #FFF;
+`;
+
+export const UsersIcon = styled(FiUsers)`
+    color: #FFF;
+`;
+
+export const StarIcon = styled(FiStar)`
+    color: #FFF;
+`;
+
+export const CompanyIcon = styled(FiBriefcase)`
+    color: #FFF;
+`;
+
+export const LocationIcon = styled(FiMapPin)`
+    color: #FFF;
+`;
+
+export const EmailIcon = styled(FiMail)`
+    color: #FFF;
+`;
+
+export const BlogIcon = styled(FiGlobe)`
+    color: #FFF;
+`;
+
+export const GitIcon = styled(FiGithub)`
+    color: #FFF;
+`;
+
+export const TwitterIcon = styled(FiTwitter)`
+    color: #FFF;
+`;
+
+export const OverviewIcon = styled(FiBookOpen)`
+    color: #FFF;
+`;
+
+export const RepositoriesIcon = styled(FiBook)`
+    color: #FFF;
+`;
+
+export const ProjectsIcon = styled(FiLayout)`
+    color: #FFF;
+`;
+
+export const PackagesIcon = styled(FiBox)`
+    color: #FFF;
+`;
+
+
+export const SvgBG = styled.svg`
+    aspect-ratio: 960/540;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    
+    position: absolute;
+    filter: opacity(60%) blur(1px);
+`;
+
+export const Input = styled.input`
+    width: 95%;
+    padding: 15px 30px;
+    background-color: rgba(27, 23, 50, 0.3);
+    outline: 1px solid #90a0d1;
+    z-index: 99;
+    border-radius: 24px;
+    color: #FFFFFF;
+    font-weight: bold;
+    font-size: 16px;
+    transition: 0.2s ease;
+    box-shadow: 0 0 1.5em rgba(0, 0, 0, 0.2);
+    border: none;
+    text-align: center;
+    margin-top: 10px;
+
+    ::placeholder{
+        color: #90a0d1;
+        font-weight: normal;
+    }
+
+    &:focus{
+        box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.2);
+        background-color: rgba(27, 23, 50, 0.1);
+        outline: 2px solid #90a0d1;
+    }
+    &:focus::placeholder{
+        /* font-weight: bold; */
     }
 `;
