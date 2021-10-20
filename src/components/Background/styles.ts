467f29ecd529bd562cdc092b048f8e72e5925c08
JPLeopolino <leopoldino26@gmail.com>
2021-10-20 15:57:46 -0300

    feat: componentized the background svg

---
+++ src/components/Background/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SvgBG = styled.svg`
+    aspect-ratio: 960/540;
+    width: 100vw;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    position: absolute;
+    filter: opacity(40%) blur(1px);
+`;
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -12,34 +12,13 @@ export const Container = styled.div`
     align-items: center;
     background-color: rgb(13,9,24);
     box-shadow: inset 0 0 8em rgba(0, 0, 0, 1);
-    /* background-color: #362B48; */
-    /* background: rgb(13,9,24);
-    background: linear-gradient(0deg, rgba(13,9,24,1) 0%, rgba(25,17,51,1) 100%); */
-    /* background-color: #0d0f17; */
+`;
 
-    /* &::after{
-        content: '';
-        position: absolute;
-        top: -50px;
-        left: -50px;
-        width: 1200px;
-        height: 1200px;
-        border-radius: 100%;
-        background: rgb(18,11,129);
-        background: linear-gradient(0deg, rgba(18,11,129,1) 0%, rgba(33,5,126,1) 49%, rgba(145,23,232,1) 100%);
-    }
-
-    &::before{
-        content: '';
-        position: absolute;
-        top: 50px;
-        right: -50px;
-        width: 800px;
-        height: 800px;
-        border-radius: 100%;
-        background: rgb(18,11,129);
-        background: linear-gradient(0deg, rgba(18,11,129,1) 0%, rgba(33,5,126,1) 49%, rgba(145,23,232,1) 100%);
-    } */
+export const Title = styled.h1`
+    color: #FFF;
+    font-size: 42px;
+    margin-bottom: 0.5em;
+    z-index: 99;
 `;
 
 export const Button = styled.button`
@@ -57,7 +36,6 @@ export const Button = styled.button`
     border: none;
 
     &:hover{
-        /* padding: 9px 35px; */
         box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.2);
         outline: 2px solid #90a0d1;
         background-color: rgba(27, 23, 50, 0.1);
@@ -70,98 +48,4 @@ export const Button = styled.button`
     &:active{
         box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.5);
     }
-`;
-
-export const Input = styled.input`
-    padding: 10px 30px;
-    background-color: rgba(27, 23, 50, 0.3);
-    outline: 1px solid #90a0d1;
-    z-index: 99;
-    border-radius: 24px;
-    color: #FFFFFF;
-    font-weight: bold;
-    font-size: 16px;
-    transition: 0.2s ease;
-    box-shadow: 0 0 1.5em rgba(0, 0, 0, 0.2);
-    border: none;
-    text-align: center;
-
-    ::placeholder{
-        color: #90a0d1;
-        font-weight: normal;
-    }
-
-    &:focus{
-        box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.2);
-        background-color: rgba(27, 23, 50, 0.1);
-        outline: 2px solid #90a0d1;
-    }
-    &:focus::placeholder{
-        /* font-weight: bold; */
-    }
-`;
-
-export const Icon = styled(BsGithub)`
-    color: #17102D;
-    position: absolute;
-    font-size: 130rem;
-    z-index: 10;
-    top: -40%;
-`;
-
-export const Title = styled.h1`
-    color: #FFF;
-    font-size: 42px;
-    margin-bottom: 0.5em;
-    z-index: 99;
-`;
-
-export const UserInfoContainer = styled.form`
-    padding: 5em 3em;
-    overflow: hidden;
-    /* background:  #1B1732; */
-    background: rgba(27, 23, 50, 0.75);
-    box-shadow: 0 0 2em rgba(0, 0, 0, 0.3);
-    /* background: rgb(18,11,129);
-    background: linear-gradient(0deg, rgba(18,11,129,1) 0%, rgba(33,5,126,1) 49%, rgba(145,23,232,1) 100%); */
-    display: flex;
-    flex-direction: column;
-    gap: 5em;
-    align-items: center;
-    justify-content: space-evenly;
-    border-radius: 18px;
-    border: 1px solid #90a0d1;
-    z-index: 999;
-    backdrop-filter: blur(24px);
-    position: relative;
-
-    &::before{
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        box-shadow: inset 0 0 3em rgba(144, 160, 209, 0.08);
-    }
-`;
-
-export const UserName = styled.p`
-    color: #FFF;
-    font-size: 18px;
-`;
-
-export const UserAvatar = styled.img`
-    width: 200px;
-    height: auto;
-    border-radius: 100%;
-    border: 2px solid #90a0d1;
-`;
-
-export const SvgBG = styled.svg`
-    aspect-ratio: 960/540;
-    width: 100vw;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    position: absolute;
-    filter: opacity(40%) blur(1px);
 `;
